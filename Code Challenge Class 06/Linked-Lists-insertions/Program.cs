f0b766b88bce84c0605c394199540b6dcc588279
shadilios <70282602+shadilios@users.noreply.github.com>
2022-03-31 01:51:50 +0300

    Linked list insertions

---
+++ Code Challenge Class 06/Linked-Lists-insertions/Program.cs
@@ -0,0 +1,209 @@
+﻿using System;
+
+namespace Linked_Lists_insertions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1- Append
+            //2- InsertBefore
+            //3- InsertAfter
+
+            MyList list = new MyList();
+            list.AddToEnd(1);
+            list.AddToEnd(2);
+            list.AddToEnd(3);
+            list.AddToEnd(4);
+            list.AddToEnd(5);
+            list.AddToEnd(6);
+            list.AddToEnd(7);
+
+            list.InsertAfter(99, 2);
+
+            list.Print();
+            
+
+        }
+    }
+
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node(int data)
+        {
+            this.data = data;
+            next = null;
+        }
+
+        public void Print()
+        {
+            Console.Write("|" + data + "|->");
+            if (next != null)
+            {
+                next.Print();
+            }
+        }
+
+        public void InsertBefore(int data, int value)
+        {
+            if (next == null)
+            {
+                next = new Node(data);
+            }
+            else if (next.data == value)
+            {
+                Node temp = new Node(data);
+                temp.next = this.next;
+                this.next = temp;
+            }
+            else
+            {
+                next.InsertBefore(data, value);
+            }
+        }
+
+        public void InsertAfter(int data, int value)
+        {
+            if (next == null)
+            {
+                next = new Node(data);
+            }
+            else if(next.data == value)
+            {
+                Node temp = new Node(data);
+                temp.next = this.next.next;
+                this.next.next = temp;
+            }
+            else
+            {
+                next.InsertAfter(data, value);
+            }
+        }
+
+        public void AddSorted(int data)
+        {
+            if (next == null)
+            {
+                next = new Node(data);
+            }
+            else if (data < next.data)
+            {
+                Node temp = new Node(data);
+                temp.next = this.next;
+                this.next = temp;
+            }
+            else
+            {
+                next.AddSorted(data);
+            }
+        }
+
+        public void AddToEnd(int data)
+        {
+            if (next == null)
+            {
+                next = new Node(data);
+            }
+            else
+            {
+                next.AddToEnd(data);
+            }
+        }
+    }
+
+    public class MyList
+    {
+        public Node headNode;
+
+        public MyList()
+        {
+            headNode = null;
+        }
+
+        //data is what we add, value what we compare with
+        public void InsertBefore(int data, int value)
+        {
+            if (headNode == null)
+            {
+                headNode = new Node(data);
+            }
+            else if(value == headNode.data)
+            {
+                AddToBeginning(data);
+            }
+            else
+            {
+                headNode.InsertBefore(data, value);
+            }
+        }
+
+        public void InsertAfter(int data, int value)
+        {
+            if (headNode == null)
+            {
+                headNode = new Node(data);
+            }
+            else if(value == headNode.data)
+            {
+                Console.WriteLine("IDK HOW DO I ADD AFTER FIRST ELEMENT");
+            }
+            else
+            {
+                headNode.InsertAfter(data, value);
+            }
+        }
+
+        public void AddToEnd(int data)
+        {
+            if (headNode == null)
+            {
+                headNode = new Node(data);
+            }
+            else
+            {
+                headNode.AddToEnd(data);
+            }
+        }
+
+        public void AddSorted(int data)
+        {
+            if (headNode == null)
+            {
+                headNode = new Node(data);
+            }
+            else if (data < headNode.data)
+            {
+                AddToBeginning(data);
+            }
+            else
+            {
+                headNode.AddSorted(data);
+            }
+        }
+
+        public void AddToBeginning(int data)
+        {
+            if (headNode == null)
+            {
+                headNode = new Node(data);
+            }
+            else
+            {
+                Node temp = new Node(data);
+                temp.next = headNode;
+                headNode = temp;
+            }
+        }
+
+        public void Print()
+        {
+            if (headNode!= null)
+            {
+                headNode.Print();
+            }
+        }
+    }
+}
---
+++ Code Challenge Class 06/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("40600f9e-1f49-49c5-9ed1-bda47309476d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
