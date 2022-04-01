51fdf88417e37b97bf2742861acce63f70d904a1
shadilios <70282602+shadilios@users.noreply.github.com>
2022-04-01 20:00:21 +0300

    array-binary-seach-fixed

--- problem03-array-binary-search/array-binary-search/Program.cs
+++ problem03-array-binary-search/array-binary-search/Program.cs
@@ -6,18 +6,25 @@ namespace array_binary_search
     {
         static void Main(string[] args)
         {
-            /*
+            
             int[] myArray = { 11, 22, 33, 44, 55, 66, 77 };
-            int key = 15;
+            int key = 55;
             Console.WriteLine(BinarySearch(myArray, key)); 
-            */
+            
         }
 
         static int BinarySearch(int[] sortedArr, int key)
         {
+            //set 2 pointers left and right, and search values between them
+            //and change left and right values each time we don't find the element
+
             //cache the passed values
             int[] myPassedArray = sortedArr;
             int searchedValue = key;
+
+            //Assign left and right value
+            int left = 0;
+            int right = myPassedArray.Length - 1;
 
             //default value if it doesn't exist
             int myIndex = -1;
@@ -26,21 +33,28 @@ namespace array_binary_search
             //if it exists in the array, return its index
             //if not return -1
 
-            for (int i = 0; i < myPassedArray.Length; i++)
+            while (left <= right)
             {
-                if (myPassedArray[i] == searchedValue)
-                {
-                    myIndex = i;
+                int middle = (left + right) / 2;
 
+                if (key == myPassedArray[middle])
+                {
+                    myIndex = middle;
                     break;
                 }
-                else
+                else if(key > myPassedArray[middle])
                 {
-                    myIndex = -1;
+                    left = middle + 1;
                 }
-            }
+                else if (key < myPassedArray[middle])
+                {
+                    right = middle - 1;
+                }
 
+            }
+            
             return myIndex;
+
         }
     }
 }
